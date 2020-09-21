f406b8d1542b9b9a99f5ed609f90a787efb83a29
German <polyakgerman@gmail.com>
2020-09-21 19:29:02 +0300

    [Cross] - add gamete & reproduce funcs

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # bio
+
+1. How many crosses should be made? 
+2. Which individuals should be selected to make the crosses?
+3. How should the selected individuals be paired up?
+4. How many progeny should each cross produce?
--- cross.py
+++ cross.py
@@ -1,24 +1,76 @@
-from typing import Union
+from typing import Union, List
 import numpy as np
+from random import choices
 
 from .models import Animal, Bull, Cow, Genotype, Phenotype
 
-
-def get_animal_cross(first: Animal, second: Animal) -> Union[Animal, Cow, Bull]:
+def get_genotype_indexies(r: List[float]) -> List[int]:
     """
-    Define new animal(genotype) created by crossbreeding first & second animals.
-    :return:
+    Get indexies for choising haplotype in genotype
+    """
+    return_list = list()
+    for i in range(1, len(r) + 1):
+        if i == 1:
+            value = choices((0, 1), weights=(0.5, 0.5))[0]
+        else:
+            value = choices((return_list[i - 1], 1 - return_list[i - 1]), weights=(1 - r[i - 1], r[i - 1]))[0]
+
+        return_list.append(value)
+
+    return return_list
+
+
+def get_gamete(genotype: List[List[int]], indexies: List[int]) -> List[int]:
+    """
+    Return gamete from genotype input
+    """
+    return [
+        genotype[i][indexies[i]] for i in range(len(genotype[0]))
+    ]
+
+def get_reproduce(genotypes: List[List[List[int]]], possibilities: List[float], population_of_progeny: int) -> List[List[List[int]]]:
+    """
+    Return children(which is amount=population_of_progeny)' genotype from parents' genotypes
+    """
+    haplotypes = [[[]]]
+    for i in range(len(genotypes[0])):
+        for j in range(len(genotypes)):
+            for k in range(population_of_progeny):
+                haplotypes[i][j][k] = get_gamete(genotypes[j], get_genotype_indexies(possibilities))
+    return haplotypes
+
+def gebv_select(reproduce: List[List[List[int]]], possibilities: List[float]):
+    return
+
+def pcv_select():
+    return
+
+def ohv_select():
+    return
+
+def wgebv_select():
+    return
+
+def ie312_select():
+    return
+
+def ie534_select():
+    return
+
+def ie634_select():
+    return
+
+def select(reproduce: List[List[List[int]]], possibilities: List[float], strategy: str='gebv') -> (int, int):
     """
 
-    new_genotype = Genotype(first.genotype.left, second.genotype.right)
-    return Bull(new_genotype)
+    Parameters
+    ----------
+    strategy: str, gebv, pcv, ohv, wgebv, ie312, ie534, ie634
 
+    Returns
+    -------
 
-def get_phenotype(animal: Animal) -> Phenotype:
     """
-    Define phenotype of animal by its genotype.
-    """
-    phenotype_value = np.mean(animal.genotype.left + animal.genotype.right)
-    return Phenotype(phenotype_value)
-
+    if strategy == 'gebv':
+        return gebv_select(reproduce, possibilities)
 
